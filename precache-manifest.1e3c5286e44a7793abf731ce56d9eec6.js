2b8c1bd5d875f3c74bef82621e108a3a3d465b9a
unknown <vprakashthapliyal@gmail.com>
2020-03-07 08:23:00 +0530

    Updates

---
+++ precache-manifest.1e3c5286e44a7793abf731ce56d9eec6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2512c7ce3e0cff02675e5e6b22f4261",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "d0c42f035fd089b283b0",
+    "url": "/Monsters-Rolodex/static/css/main.1079d982.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/Monsters-Rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Monsters-Rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c42f035fd089b283b0",
+    "url": "/Monsters-Rolodex/static/js/main.0573c9c3.chunk.js"
+  },
+  {
+    "revision": "c8b4cd4bde6cd39c3f0e",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.ea79795c.js"
+  }
+]);
